1434ecb63a98195b57c3a11e042455c377e05a1d
LMGajardo <131201064+LMGajardo@users.noreply.github.com>
2023-10-31 14:16:39 -0300

    A

--- app/src/main/java/com/example/lm_seguridad/Perfil1.java
+++ app/src/main/java/com/example/lm_seguridad/Perfil1.java
@@ -2,6 +2,7 @@ package com.example.lm_seguridad;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,11 +10,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
@@ -28,6 +27,8 @@ public class Perfil1 extends AppCompatActivity {
     private static final int PICK_IMAGE_REQUEST = 1;
     private ImageView ivFoto;
     private StorageReference storageReference;
+    private static final String PREFS_NAME = "MyPrefsFile";
+    private static final String PROFILE_IMAGE_URL_KEY = "profileImageUrl";
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -37,6 +38,20 @@ public class Perfil1 extends AppCompatActivity {
 
         ivFoto = findViewById(R.id.ivFoto);
         storageReference = FirebaseStorage.getInstance().getReference();
+
+        // Cargar la imagen del SharedPreferences al ImageView
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        String imageUrl = prefs.getString(PROFILE_IMAGE_URL_KEY, null);
+        if (imageUrl != null) {
+            // Cargar y mostrar la imagen en el ImageView con Glide
+            RequestOptions requestOptions = new RequestOptions()
+                    .diskCacheStrategy(DiskCacheStrategy.ALL);
+
+            Glide.with(this)
+                    .load(imageUrl)
+                    .apply(requestOptions)
+                    .into(ivFoto);
+        }
 
         Button buttonUpload = findViewById(R.id.buttonUpload);
         buttonUpload.setOnClickListener(new View.OnClickListener() {
@@ -73,12 +88,11 @@ public class Perfil1 extends AppCompatActivity {
                     .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                         @Override
                         public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            // La imagen se cargó exitosamente
                             taskSnapshot.getStorage().getDownloadUrl()
                                     .addOnSuccessListener(new OnSuccessListener<Uri>() {
                                         @Override
                                         public void onSuccess(Uri uri) {
-                                            // Carga y muestra la imagen en el ImageView con Glide
+                                            // Cargar y mostrar la imagen en el ImageView con Glide
                                             RequestOptions requestOptions = new RequestOptions()
                                                     .diskCacheStrategy(DiskCacheStrategy.ALL);
 
@@ -86,6 +100,11 @@ public class Perfil1 extends AppCompatActivity {
                                                     .load(uri)
                                                     .apply(requestOptions)
                                                     .into(ivFoto);
+
+                                            // Guardar la URL de la imagen en SharedPreferences
+                                            SharedPreferences.Editor editor = getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
+                                            editor.putString(PROFILE_IMAGE_URL_KEY, uri.toString());
+                                            editor.apply();
                                         }
                                     })
                                     .addOnFailureListener(new OnFailureListener() {
